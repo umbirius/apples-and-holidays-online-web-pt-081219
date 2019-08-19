8ae98e89279a9f39e97d8ab6ecd58e69462185d1
Umberto Palazzo <upalazzo93@gmail.com>
2019-08-19 19:55:27 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,10 +67,12 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday_hash.collect |season,holidays|
-    holidays.select |holidays,supplies|
+  holiday_hash.collect{ |season,holidays|
+    holidays.select{ |holiday,supplies|
       supplies.include?("BBQ")
-      }.keys.flatten
+      }.keys
+  }.flatten
+  
     
     
 
