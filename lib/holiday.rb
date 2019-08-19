a7d92794c6389d2c179e69ffc7cfa0a111c376b6
Umberto Palazzo <upalazzo93@gmail.com>
2019-08-19 19:52:43 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,6 +68,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_hash.collect |season,holidays|
+    season.select {|holidays|
     
     
 
