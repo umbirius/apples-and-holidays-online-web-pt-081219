90a7c5c14846f468b56039c038027f3851ac3b1f
Umberto Palazzo <upalazzo93@gmail.com>
2019-08-19 19:41:55 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,6 +56,12 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season,holiday|
+    puts season 
+    puts holiday
+    holiday.each do |supplies| 
+      puts supplies
+    end 
+  end 
     
 end
 
