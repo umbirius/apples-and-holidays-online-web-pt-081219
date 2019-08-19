79deafb334b1b22768c1078e0d3223736dc436e1
Umberto Palazzo <upalazzo93@gmail.com>
2019-08-19 19:45:47 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,7 +56,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season,holiday|
-    puts  "#{season.to_s.capitlize}" 
+    puts  "#{season.to_s.capitalize}" 
     holiday.each do |supplies| 
       puts "  #{holiday.to_s.split("_").collect{|w| w.capitalize}.join(" ")}: #{supplies.join(", ")}"
     end 
